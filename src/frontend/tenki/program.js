72d15f611afa77d7f65604ba85cf1a8c35453c63
Nakai Itsuki <nakai.itsuki.ul@tut.jp>
2024-09-22 17:51:55 +0900

    ソースコードにコメントを追加

--- src/frontend/tenki/program.js
+++ src/frontend/tenki/program.js
@@ -29,6 +29,7 @@ $(function () {
     }
 
     function loadjson() {
+        //各urlからjsonデータを取得する
         for (let t = 0; t < urls.length; t++) {
             try {
                 var jsonData = fetchJsonSync(urls[t][0]);
@@ -41,63 +42,72 @@ $(function () {
 
     function fetchJsonSync(url) {
         var request = new XMLHttpRequest();
-        request.open('GET', url, false);
-        request.send();
-
-        if (request.status === 200) {
-            return JSON.parse(request.responseText);
-        } else {
-            throw new Error(`Failed to fetch data: ${request.status}`);
+        request.open('GET', url, false); // 同期リクエスト
+        try {
+            request.send();
+    
+            if (request.status === 200) {
+                return JSON.parse(request.responseText);
+            } else {
+                throw new Error(`Failed to fetch data: ${request.status} ${request.statusText}`);
+            }
+        } catch (error) {
+            console.error('Error:', error.message);
+            throw error; // エラーを再度投げて上位で処理できるようにする
         }
     }
 
     function showWeather(index) {
         let nowTime = new Date();
-        let nowHour = nowTime.getHours();
-
+        let nowHour = nowTime.getHours();//時間 Hour を取得する
+        //17時以降は明日の天気を表示するようにする
         if (nowHour >= 17) {
-            updateWeatherForTomorrow(index);
+            updateWeatherForTomorrow(index);//明日の天気を表示
         } else {
-            updateWeatherForToday(index);
+            updateWeatherForToday(index);//今日の天気を表示
         }
-        animateWeatherIcon();
     }
 
+    /* 指定した地点（配列のインデックスで指定）の「今日の天気」を表示する関数*/
     function updateWeatherForToday(i) {
-        $('#asuarea').empty();
-        $('#ltmparea').empty();
-        $('#ltmparea').append('<style>#tenki{opacity:1;}#tr2{opacity:0;}#htmp{top:5px;}#hp{top:30px;}</style>');
-        $('#chiten').empty();
-        $('#chiten').append(weather_data[i].area);
-        $('#htmp').empty();
-        $('#htmp').append(Math.round(weather_data[i].htmp));
-        $('#pop1').empty();
-        $('#pop1').append(Math.round(weather_data[i].pop1));
-        $('#pop2').empty();
-        $('#pop2').append(Math.round(weather_data[i].pop2));
-        tenkimode = getWeatherMode(weather_data[i].tenki);
+        $('#asuarea').empty();//{id:asuarea}内の要素を削除する
+        $('#ltmparea').empty();//{id:ltmparea}内の要素を削除する
+        $('#ltmparea').append('<style>#tenki{opacity:1;}#tr2{opacity:0;}#htmp{top:5px;}#hp{top:30px;}</style>');//{id:ltmparea}内にCSSを追加する
+        $('#chiten').empty();//{id:chiten}内の既存の地点名を削除する
+        $('#chiten').append(weather_data[i].area);//{id:chiten}内に新規の地点名を表示する
+        $('#htmp').empty();//{id:htmp}内の要素を削除する
+        $('#htmp').append(Math.round(weather_data[i].htmp));//{id:htmp}内に最高気温を表示する
+        $('#pop1').empty();//{id:pop1}内の要素を削除する
+        $('#pop1').append(Math.round(weather_data[i].pop1));//{id:pop1}内に降水確率を表示する
+        $('#pop2').empty();//{id:pop2}内の要素を削除する
+        $('#pop2').append(Math.round(weather_data[i].pop2));//{id:pop2}内に降水確率を表示する
+        let tenkimode = getWeatherMode(weather_data[i].tenki);//気象庁のサーバから取得した天気コードを変換する
+        animateWeatherIcon(tenkimode);//天気アイコンを表示
     }
 
+    /* 指定した地点（配列のインデックスで指定）の「明日の天気」を表示する関数*/
     function updateWeatherForTomorrow(i) {
-        $('#asuarea').empty();
-        $('#asuarea').append('<div id="asu">あす</div>');
-        $('#ltmparea').empty();
+        $('#asuarea').empty();//{id:asuarea}内の要素を削除する
+        $('#asuarea').append('<div id="asu">あす</div>');//{id:asuarea}内に「あす」と表示する
+        $('#ltmparea').empty();//{id:ltmparea}内の要素を削除する
         $('#ltmparea').append('<style>#tenki{opacity:1;}#pop1{font-size:30px;right:190px;top:130px;}#pop2{font-size:30px;right:120px;top:130px;}#htmp{right:105px;}#hp{left:200px;}#tr,#tp2{opacity:0;}#tp{top:145px;right:130px;}</style>');
-        $('#ltmparea').append('<div id="lp">℃</div><div id="ltmp"></div>');
-        $('#chiten').empty();
-        $('#chiten').append(weather_data[i].area);
-        $('#htmp').empty();
-        $('#htmp').append(Math.round(weather_data[i].nhtmp));
-        $('#ltmp').empty();
-        $('#ltmp').append(Math.round(weather_data[i].nltmp));
-        $('#pop1').empty();
-        $('#pop1').append(Math.round(weather_data[i].npop1));
-        $('#pop2').empty();
-        $('#pop2').append(Math.round(weather_data[i].npop2));
-        tenkimode = getWeatherMode(weather_data[i].ntenki);
+        $('#ltmparea').append('<div id="lp">℃</div><div id="ltmp"></div>');//{id:ltmparea}内にCSSを追加する
+        $('#chiten').empty();//{id:chiten}内の既存の地点名を削除する
+        $('#chiten').append(weather_data[i].area);//{id:chiten}内に新規の地点名を表示する
+        $('#htmp').empty();//{id:htmp}内の要素を削除する
+        $('#htmp').append(Math.round(weather_data[i].nhtmp));//{id:htmp}内に明日の最高気温を表示する
+        $('#ltmp').empty();//{id:ltmp}内の要素を削除する
+        $('#ltmp').append(Math.round(weather_data[i].nltmp));//{id:ltmp}内に明日の最低気温を表示する
+        $('#pop1').empty();//{id:pop1}内の要素を削除する
+        $('#pop1').append(Math.round(weather_data[i].npop1));//{id:pop1}内に明日の降水確率を表示する
+        $('#pop2').empty();//{id:pop2}内の要素を削除する
+        $('#pop2').append(Math.round(weather_data[i].npop2));//{id:pop2}内に明日の降水確率を表示する
+        let tenkimode = getWeatherMode(weather_data[i].ntenki);//気象庁のサーバから取得した天気コードを変換する
+        animateWeatherIcon(tenkimode);//天気アイコンを表示
     }
-
-    function animateWeatherIcon() {
+    
+    /* 引数に指定された天気コードのアイコンを表示する関数*/
+    function animateWeatherIcon(tenkimode) {
         $('.mark').css('opacity', 0);
         $('#m' + tenkimode).css('opacity', 1).animate({ paddingRight: 1 }, {
             duration: 1000,
@@ -110,6 +120,7 @@ $(function () {
         });
     }
 
+    /* 気象庁のサーバから取得した天気コードを独自定義のコードに変換する関数*/
     function getWeatherMode(code) {
         const weatherMapping = {
             "100": 100, "123": 100, "124": 100, "130": 100, "131": 100,//晴れ
@@ -143,7 +154,7 @@ $(function () {
             "371": 413, "413": 413, "421": 413,//雪のちくもり
             "414": 414, "422": 414, "423": 414, "424": 414, "426": 414//雪のち雨
         };
-        return weatherMapping[code] || null;
+        return weatherMapping[code] || null;//本アプリ独自の天気コードを返す
     }
 
     function reloadWeather(data) {
@@ -188,7 +199,6 @@ $(function () {
             htmp = ltmp;
             ltmp = dd;
         }
-
         var childarray = { area: area, tenki: tenki, htmp: htmp, ltmp: ltmp, pop1: pop1, pop2: pop2, ntenki: ntenki, nhtmp: nhtmp, nltmp: nltmp, npop1: npop1, npop2: npop2 };
         weather_data[weather_data.length] = childarray;
     }
